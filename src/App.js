ffb0f3824ca3e0dacdb0280aea5c69a1e7c9fbe0
jaywomack <womackjw@hotmail.com>
2020-01-27 14:14:08 -0800

    added useritem

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
+import Navbar from '../src/components/layout/Navbar';
+import UserItem from './components/layout/users/UserItem';
 
 class App extends Component {
+    // foo = () => 'Bar'
     render() {
-        const name = 'John Doe';
-        const loading = false;
-        const showName = false;
+        // const name = 'John Doe';
+        // const loading = false;
+        // const showName = false;
 
         return (
             <Fragment>
-                <h1>Hello World</h1>
+                <Navbar />
+                <UserItem />
             </Fragment>
         );
     }
